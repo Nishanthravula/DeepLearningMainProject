79a1b8287570643eccdf39060543ac810fcaf105
Ravula Nishanth <nishanth.ravula@outlook.com>
2023-05-11 00:43:35 +0530

    Update header.py

--- projects/mmdet3d_plugin/voxformer/utils/header.py
+++ projects/mmdet3d_plugin/voxformer/utils/header.py
@@ -1,8 +1,7 @@
 # Copyright (c) 2022-2023, NVIDIA Corporation & Affiliates. All rights reserved.
 #
 # This work is made available under the Nvidia Source Code License-NC.
-# To view a copy of this license, visit
-# https://github.com/NVlabs/VoxFormer/blob/main/LICENSE
+
 
 import torch
 import torch.nn as nn
